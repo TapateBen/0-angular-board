aa628bb3e713ea04c4e168cffb4b61c11976fe5b
Adrián Lamas Silva <adrianlamassilva@gmail.com>
2019-04-03 18:43:59 +0200

    'pub'

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { InfoComponent } from './about/info/info.component';
+import { LinksComponent } from './about/links/links.component';
+import { AuthorsComponent } from './about/authors/authors.component';
+import { AuthorComponent } from './about/authors/author/author.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AboutComponent
+    component: AboutComponent,
+    children: [
+      {
+      path: 'links',
+      component: LinksComponent
+      },
+      {
+      path: 'info',
+      component: InfoComponent
+      },
+      {
+      path: 'authors', component: AuthorsComponent
+      },
+      {
+      path: 'authors/:id', component: AuthorComponent
+      }
+      ]
   }
 ];
 
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about/about.component';
+import { LinksComponent } from './about/links/links.component';
+import { InfoComponent } from './about/info/info.component';
+import { AuthorsComponent } from './about/authors/authors.component';
+import { AuthorComponent } from './about/authors/author/author.component';
 
 @NgModule({
-  declarations: [AboutComponent],
+  declarations: [AboutComponent, LinksComponent, InfoComponent, AuthorsComponent, AuthorComponent],
   imports: [
     CommonModule,
     AboutRoutingModule
